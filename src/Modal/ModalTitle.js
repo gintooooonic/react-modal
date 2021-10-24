218c185940bbc511c491e144330e4eaf90e2c1c6
Woojin Shin <gintooooonic@gmail.com>
2021-10-24 19:40:50 +0900

    .

--- src/Modal/ModalTitle.js
+++ src/Modal/ModalTitle.js
@@ -15,11 +15,22 @@ const ModalTitleDiv = styled.div`
   left: 0;
   right: 0;
   height: 30px;
+  border-radius: 10px 10px 0 0;
   & > p {
     margin: 0;
     line-height: 30px;
     font-size: 15px;
     font-weight: bold;
+  }
+  & * {
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+  }
+  &:hover {
+    background-color: #e6e6e6;
   }
 `;
 
@@ -31,4 +42,8 @@ const CloseButton = styled.div`
   height: 15px;
   border-radius: 50%;
   background-color: #ff5f57;
+
+  &:hover {
+    background-color: #ff3d33;
+  }
 `;
